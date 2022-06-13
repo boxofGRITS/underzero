7149cf1aea2225df68063999bc9f99ba4f62f4a5
Elijah Davis <93060246+samuraicupcake66@users.noreply.github.com>
2022-06-12 19:04:28 -0500

    first commit

---
+++ assets/preload/stages/leostage1.lua
@@ -0,0 +1,39 @@
+function onCreate()
+	--Background stuff
+	makeLuaSprite('LeoBG1','Stage 1',0, 0);
+	addLuaSprite('LeoBG1',false);
+	scaleObject('LeoBG1', 0.8, 0.8);
+	setLuaSpriteScrollFactor('LeoBG1', 1, 1);
+	close(true);
+end
+
+function onUpdate(elapsed)
+
+end
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )-- for every step
+	-- body
+end
+
+function onUpdate( ... )
+    -- body
+end
+
+
+
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )-- for every step
+	-- body
+end
+
+function onUpdate( ... )
+    -- body
+end
---
+++ assets/preload/stages/leostage2.lua
@@ -0,0 +1,39 @@
+function onCreate()
+	--Background stuff
+	makeLuaSprite('LeoBG2','Stage 2',0, 0);
+	addLuaSprite('LeoBG2',false);
+	scaleObject('LeoBG2', 0.8, 0.8);
+	setLuaSpriteScrollFactor('LeoBG2', 1, 1);
+	close(true);
+end
+
+function onUpdate(elapsed)
+
+end
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )-- for every step
+	-- body
+end
+
+function onUpdate( ... )
+    -- body
+end
+
+
+
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )-- for every step
+	-- body
+end
+
+function onUpdate( ... )
+    -- body
+end
